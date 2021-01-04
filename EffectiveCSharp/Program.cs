68464a5ca7efdbf95ed1caaf242e2b7d471f4279
zhixin <zhixin@zhixins-MacBook-Pro.local>
2021-01-04 22:33:57 +0800

    1.4 init

---
+++ EffectiveCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EffectiveCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
